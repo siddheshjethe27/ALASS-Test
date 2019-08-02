a3dd81021bee4702b3a5e9423b7c9281c1315334
SIJETHE <SIJETHE@MLB-PME-L0025L.fll.ad.emb>
2019-08-02 11:27:38 -0400

    Added BO

---
+++ test/src/main/java/com/FIT/test/businessObjects/Data.java
@@ -0,0 +1,15 @@
+package com.FIT.test.businessObjects;
+
+public class Data 
+{
+		String input;
+		String output;
+		public String getInput() {
+			return input;
+		}
+		public void setInput(String input) {
+			this.input = input;
+		}
+		
+}
+
